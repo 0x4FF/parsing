57d27074fc60be0837409d2ae673c8548ed1e4b5
1waves <112463082+1waves@users.noreply.github.com>
2022-09-21 23:48:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,27 @@
 #include <fstream>
 #include <string>
 
-std::fstream file_to_parse, file_to_write;
-std::string file_line;
-int numbers;
-
-
-void parse(char* argv[])
+int main(int argc, char * argv[])
 {
     std::string file_name = argv[1];
-    file_to_parse >> numbers;file_to_parse.close();
-    file_to_write.open(file_name);
-    file_to_write << "{\n" "\t\"Key:\": " << "" << "\"" << file_line << "\"," << "\n" << "\t\"Value:\": " << "\"" << numbers << "\"" << "\n}" << std::endl;
-    file_to_write.close();
-}
-
-int main(void)
-{
-   
+    std::fstream file_to_parse, file_to_write;
+    std::string file_line;
+    int numbers;
+    file_to_parse.open(file_name);
+    
     if (!file_to_parse.is_open()) { std::cout <<"Error opening file"; }
     else if (file_to_parse.is_open())
     {
-        while(getline(file_to_parse, file_line, ':'))
+
+        while(file_to_parse)
         {
-            parse(NULL);
+            
+            getline(file_to_parse, file_line, ':');
+            file_to_parse >> numbers;file_to_parse.close();
+            file_to_write.open(file_name);
+            file_to_write << "{\n" "\t\"Key:\": " << "" << "\"" << file_line << "\"," << "\n" << "\t\"Value:\": " << "\"" << numbers << "\"" << "\n}" << std::endl;
+            file_to_write.close();
+    
         }
 
     }
