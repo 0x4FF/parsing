35bdcb2812e3f85529ee8130bc4dd0443aa00741
0xc00000 <112463082+0xc00000@users.noreply.github.com>
2022-10-03 14:01:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) { 
     std::string file_name = argv[1];
     std::fstream file_to_parse, file_to_write;
     std::string file_line;
@@ -11,18 +10,13 @@ int main(int argc, char * argv[])
     file_to_parse.open(file_name);
     
     if (!file_to_parse.is_open()) { std::cout <<"Error opening file"; }
-    else if (file_to_parse.is_open())
-    {
-
-        while(file_to_parse)
-        {
-            
+    else if (file_to_parse.is_open()) {
+        while(file_to_parse) {
             getline(file_to_parse, file_line, ':');
             file_to_parse >> numbers;
             file_to_write.open(file_name);
             file_to_write << "{\n" "\t" "" << "" << "\"" << file_line << "\"" <<  ":  " << "\"" << numbers << "\"" << "\n" << "}";
             file_to_write.close();file_to_parse.close();
-    
         }
 
     }
